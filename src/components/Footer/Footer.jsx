14215f180dcb8e28e39a415f8d35b33c87393c23
truong_nhat <nhattruong16062003@gmail.com>
2025-04-08 15:00:48 +0700

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 import "./Footer.css";
 import { checkData } from "../../helpers/encryptionHelper";
 import { useUserData } from "../../context/UserDataProvider";
+import { useNavigate } from "react-router-dom";
 
 const Footer = React.memo(() => {
     const [validRole, setValidRole] = useState(false);
     const { isLoggedIn } = useUserData();
+    const navigate = useNavigate();
+
     useEffect(() => {
         const fetchRole = async () => {
             if (isLoggedIn) {
@@ -40,9 +43,10 @@ const Footer = React.memo(() => {
 
                 <aside className="footer-center" role="table">
                     <h4 className="footer-title" role="complementary">Cộng đồng</h4>
-                    <h6>
-                        <Link to="/register-artist" className="register-link">Dành cho các nghệ sĩ</Link>
-                    </h6>
+                    <h6
+                        onClick={() => navigate("/register-artist")}
+
+                    >Đăng ký tài khoản nghệ sĩ</h6>
                     <h6>Nhà phát triển</h6>
                     <h6>Quảng cáo</h6>
                     <h6>Nhà đầu tư</h6>
